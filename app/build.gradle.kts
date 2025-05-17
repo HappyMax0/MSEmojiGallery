47d4635f616525c95fcf825e934e105d4f2a7aa8
HappyMax0 <Jihaoran12@hotmail.com>
2025-05-17 22:16:22 +0800

    1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.happymax.msemojigallery"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
